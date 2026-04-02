92d8db30f38bc1ff50324d5ba233dbe163699362
Veer <veerk.dev@gmail.com>
2026-04-02 18:00:06 +0530

    Unfinished SignatureCell and Storyboard

--- Axiomora/Features/Signatures/SignaturesViewController.swift
+++ Axiomora/Features/Signatures/SignaturesViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class SignaturesViewController: UIViewController {
 
-    @IBOutlet weak var createNewSignatureButton: UIButton!
+    @IBOutlet weak var emptySignaturesStackView: UIStackView!
     @IBOutlet weak var addBarButtonItem: UIBarButtonItem!
+    @IBOutlet weak var selectButton: UIBarButtonItem!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private var signatures: [Signature] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupUI()
     }
     
     @IBAction func addButtonTapped(_ sender: Any) {
@@ -23,6 +29,22 @@ class SignaturesViewController: UIViewController {
     
     @IBAction func createNewSignatureTapped(_ sender: UIButton) {
         launchNewSignatureViewController(sourceView: sender)
+    }
+    
+    private func setupUI() {
+        addBarButtonItem.style = .prominent
+//        selectButton.style = .prominent
+        updateEmptyState()
+    }
+    
+    private func updateEmptyState() {
+        
+        let isEmpty = signatures.isEmpty
+        emptySignaturesStackView.isHidden = !isEmpty
+        selectButton.isHidden = isEmpty
+        editButton.isHidden = isEmpty
+        collectionView.isHidden = isEmpty
+        
     }
     
     private func launchNewSignatureViewController(sourceView: UIView?) {
@@ -35,4 +57,24 @@ class SignaturesViewController: UIViewController {
         
         present(navController, animated: true)
     }
+}
+
+extension SignaturesViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return signatures.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+}
+
+extension SignaturesViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
 }
