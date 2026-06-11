54c66cd3f5b5e01acdf8ced30fef3a78b5fa60bc
Kush Sehgal <123003893+sehgal18505@users.noreply.github.com>
2026-06-11 14:33:44 +0530

    UI Fixed

--- Axiomora/App/SceneDelegate.swift
+++ Axiomora/App/SceneDelegate.swift
@@ -15,6 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
+        window.overrideUserInterfaceStyle = .dark
         self.window = window
         
         let hasSeenOnboarding = UserDefaults.standard.bool(forKey: "hasSeenOnboarding")
